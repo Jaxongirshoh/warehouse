c6665dd63890b9e0fabee3a7e6528994ceae58f0
wisespirit <@Jaxongirshoh>
2024-11-03 20:30:55 +0500

    commit

--- src/main/java/dev/wisespirit/warehouse/WarehouseApplication.java
+++ src/main/java/dev/wisespirit/warehouse/WarehouseApplication.java
@@ -3,6 +3,12 @@ package dev.wisespirit.warehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This simple class demonstrate Spring boot main class
+ * @version 0.0
+ * @author wisespirit
+ */
+
 @SpringBootApplication
 public class WarehouseApplication {
 
--- src/test/java/dev/wisespirit/warehouse/WarehouseApplicationTests.java
+++ src/test/java/dev/wisespirit/warehouse/WarehouseApplicationTests.java
@@ -3,6 +3,12 @@ package dev.wisespirit.warehouse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * This simple class demonstrate test of WarehouseApplication class
+ * @version 0.0
+ * @author wisespirit
+ */
+
 @SpringBootTest
 class WarehouseApplicationTests {
 
