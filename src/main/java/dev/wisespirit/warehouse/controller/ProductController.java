d4fa5256d928bf0a458bfa9e4a05e95e06c9638b
Ozodbek <@os4025966~>
2024-11-16 20:21:50 +0500

    hawk branch

---
+++ src/main/java/dev/wisespirit/warehouse/controller/ProductController.java
@@ -0,0 +1,4 @@
+package dev.wisespirit.warehouse.controller;
+
+public class ProductController {
+}
---
+++ src/main/java/dev/wisespirit/warehouse/repository/ProductRepository.java
@@ -0,0 +1,4 @@
+package dev.wisespirit.warehouse.repository;
+
+public class ProductRepository {
+}
